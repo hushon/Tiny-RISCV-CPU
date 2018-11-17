f6d665746796b0733f39327d6d10f29ac1288d89
hkshon <hyounguk.shon@gmail.com>
2018-11-17 19:27:18 +0900

    hazarddetect implement

--- RISC-V_PipelineCPU-lab/Forwarding.v
+++ RISC-V_PipelineCPU-lab/Forwarding.v
@@ -10,27 +10,36 @@
 		output reg [1:0] forwardB
 		);
 
+		initial begin
+			forwardA = 2'bxx;
+			forwardB = 2'bxx;
+		end
+
 		always @(*) begin
 
 			if ((rs_ex!=0) && ((rs1_ex==rd_mem)||(rs2_ex==rd_mem)) && RegWrite_mem) begin // forward operand from MEM stage
-				if (rs1_ex==rd_mem) begin
+				forwardA = (rs1_ex==rd_mem)? 2'b01 : 2'b00;
+				forwardB = (rs2_ex==rd_mem)? 2'b01 : 2'b00;
+				/* if (rs1_ex==rd_mem) begin
 					forwardA = 2'b01;
 					forwardB = 2'b00;
 				end
 				else if (rs2_ex==rd_mem) begin
 					forwardA = 2'b00;
 					forwardB = 2'b01;
-				end
+				end */
 			end
 			else if ((rs_ex!=0) && ((rs1_ex==rd_wb)||(rs2_ex==rd_wb)) && RegWrite_wb) begin // forward operand from WB stage
-				if (rs1_ex==rd_wb) begin
+				forwardA = (rs1_ex==rd_wb)? 2'b01 : 2'b00;
+				forwardB = (rs2_ex==rd_wb)? 2'b01 : 2'b00;
+				/* if (rs1_ex==rd_wb) begin
 					forwardA = 2'b10;
 					forwardB = 2'b00;
 				end
 				else if (rs2_ex==rd_wb) begin
 					forwardA = 2'b00;
 					forwardB = 2'b10;
-				end
+				end */
 			end
 			else begin // use the operand from register file
 				forwardA = 2'b00;
--- RISC-V_PipelineCPU-lab/Hazard_detect.v
+++ RISC-V_PipelineCPU-lab/Hazard_detect.v
@@ -9,4 +9,23 @@
 		output reg IF_ID_Write
 		);
 
+		initial begin
+			load_delay = 1'bx;
+			PCWrite = 1'bx;
+			IF_ID_Write = 1'bx;
+		end
+
+		always @(*) begin
+			if (((rs1_id==rd_ex)||(rs2_id==rd_ex)) && MemRead_ex) begin
+				load_delay = 1;
+				PCWrite = 0;
+				IF_ID_Write = 0;
+			end
+			else begin
+				load_delay = 0;
+				PCWrite = 1;
+				IF_ID_Write = 1;
+			end
+		end
+		
 	endmodule
