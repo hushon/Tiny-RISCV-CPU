8f6a01f89e45a123f5a0d688246dce6704b4a3d6
hkshon <hyounguk.shon@gmail.com>
2018-11-18 14:29:37 +0900

    changed forwarding

--- RISC-V_PipelineCPU-lab/Forwarding.v
+++ RISC-V_PipelineCPU-lab/Forwarding.v
@@ -17,32 +17,25 @@
 
 		always @(*) begin
 
-			if (((rs1_ex==rd_mem)||(rs2_ex==rd_mem)) && RegWrite_mem) begin // forward operand from MEM stage
-				forwardA = (rs1_ex==rd_mem)? 2'b01 : 2'b00;
-				forwardB = (rs2_ex==rd_mem)? 2'b01 : 2'b00;
-				/* if (rs1_ex==rd_mem) begin
-					forwardA = 2'b01;
-					forwardB = 2'b00;
-				end
-				else if (rs2_ex==rd_mem) begin
-					forwardA = 2'b00;
-					forwardB = 2'b01;
-				end */
+			// forwardA signal
+			if ((rs1_ex==rd_mem) && RegWrite_mem) begin // forward operand from MEM stage
+				forwardA = 2'b01;
 			end
-			else if (((rs1_ex==rd_wb)||(rs2_ex==rd_wb)) && RegWrite_wb) begin // forward operand from WB stage
-				forwardA = (rs1_ex==rd_wb)? 2'b01 : 2'b00;
-				forwardB = (rs2_ex==rd_wb)? 2'b01 : 2'b00;
-				/* if (rs1_ex==rd_wb) begin
-					forwardA = 2'b10;
-					forwardB = 2'b00;
-				end
-				else if (rs2_ex==rd_wb) begin
-					forwardA = 2'b00;
-					forwardB = 2'b10;
-				end */
+			else if ((rs1_ex==rd_wb) && RegWrite_wb) begin // forward operand from WB stage
+				forwardA = 2'b10;
 			end
 			else begin // use the operand from register file
 				forwardA = 2'b00;
+			end
+
+			// forwardB signal
+			if ((rs2_ex==rd_mem) && RegWrite_mem) begin // forward operand from MEM stage
+				forwardB = 2'b01;
+			end
+			else if ((rs2_ex==rd_wb) && RegWrite_wb) begin // forward operand from WB stage
+				forwardB = 2'b10;
+			end
+			else begin // use the operand from register file
 				forwardB = 2'b00;
 			end
 
